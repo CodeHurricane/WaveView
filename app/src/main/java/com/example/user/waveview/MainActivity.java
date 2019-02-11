32f2425a32aa8566e6e986d0d2d74515a5d20c78
pengfeisun <pengfeisun122@163.com>
2019-02-11 13:20:59 +0800

    first

---
+++ app/src/main/java/com/example/user/waveview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.user.waveview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	 	final WaveView waveView = (WaveView) findViewById(R.id.waveView);
+	         waveView.postDelayed(new Runnable() {
+				 @Override
+				 public void run() {
+					 waveView.startAnimation();
+				 }
+			 },1000);
+	}
+}
